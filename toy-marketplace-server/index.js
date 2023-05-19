b26d407c2e1c2b6f17f73f0f2bdc7dd83ffe3dac
Md Alim <mdalimoattabur@gmail.com>
2023-05-20 00:25:33 +0600

    get show details spacifik data

--- toy-marketplace-server/index.js
+++ toy-marketplace-server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+// middleware
+
+app.use(cors())
+app.use(express())
 
 app.get('/', (req, res)=>{
     res.send('toy server is running......')
@@ -36,8 +43,15 @@ async function run() {
 
     app.get('/category/:title', async(req, res)=>{
         const title= req.params.title;
-        const query = {subCategoryName : title}
+        const query = {category : title}
         const result = await categoryDataCollection.find(query).toArray();
+        res.send(result)
+    })
+
+    app.get('/shopDetails/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id  : new ObjectId(id)};
+        const result = await categoryDataCollection.findOne(query);
         res.send(result)
     })
 
