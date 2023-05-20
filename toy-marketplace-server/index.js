46688f01e0a6e5ed8ee513b790d2d32aaa2c4f7d
Md Alim <mdalimoattabur@gmail.com>
2023-05-20 17:43:31 +0600

    delete single toys

--- toy-marketplace-server/index.js
+++ toy-marketplace-server/index.js
@@ -73,6 +73,15 @@ async function run() {
     })
 
 
+    // delete a toy
+
+    app.delete('/myToys/toyRemove/:id' , async(req, res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id) }
+      const result = await categoryDataCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
